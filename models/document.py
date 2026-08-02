46f9c12493c26c0b0db012dc70b72da634cb23f2
juliobugfixer <ngmanuel21@gmail.com>
2026-08-02 17:54:54 +0100

    style(models): Remover comentarios

--- models/document.py
+++ models/document.py
@@ -8,7 +8,7 @@ class Document(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     title = db.Column(db.String(200), nullable=False)
     file_name = db.Column(db.String(255))
-    file_type = db.Column(db.String(50), nullable=False) # 'text', 'pdf', 'docx'
+    file_type = db.Column(db.String(50), nullable=False)
     file_path = db.Column(db.String(255))
     content = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
